4e341bacd715a48f19622fd913f5569fd89cdfc6
zkan <zkan.cs@gmail.com>
2021-04-18 14:26:25 +0700

    Play with image :bear:

--- strings-and-images.rkt
+++ strings-and-images.rkt
@@ -1,6 +1,8 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname strings-and-images) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
+
 "apple"
 
 (string-append "Ada" " Lovelsace")
@@ -16,3 +18,19 @@
 (substring "012345678" 2 4)
 
 (substring "Caribou" 0 3)
+
+(circle 10 "solid" "red")
+(rectangle 30 60 "outline" "blue")
+(text "hello" 24 "orange")
+
+(above (circle 10 "solid" "red")
+       (circle 20 "solid" "yellow")
+       (circle 30 "solid" "green"))
+
+(beside (circle 10 "solid" "red")
+        (circle 20 "solid" "yellow")
+        (circle 30 "solid" "green"))
+
+(overlay (circle 10 "solid" "red")
+         (circle 20 "solid" "yellow")
+         (circle 30 "solid" "green"))
