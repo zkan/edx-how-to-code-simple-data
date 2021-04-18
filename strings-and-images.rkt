cf460b52900f6237f3b72dc67bf9a9e965daaed9
zkan <zkan.cs@gmail.com>
2021-04-18 13:57:50 +0700

    Play with strings :bear:

---
+++ strings-and-images.rkt
@@ -0,0 +1,18 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname strings-and-images) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+"apple"
+
+(string-append "Ada" " Lovelsace")
+
+(+ 1 123)
+
+; (+ 1 "123") ; Error
+
+(string-length "Apple")
+
+(substring "Caribou" 2 4) ; zero-based indexing
+
+(substring "012345678" 2 4)
+
+(substring "Caribou" 0 3)
