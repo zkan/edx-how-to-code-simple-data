1326fbc657ae392b3deb035baf93c5658be9bbff
zkan <zkan.cs@gmail.com>
2021-05-09 08:21:59 +0700

    Play with HtDD :bear:

---
+++ city-name-starter.rkt
@@ -0,0 +1,14 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname city-name-starter) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; CityName is String
+;; interp. the name of a city
+
+(define CN1 "Bangkok")
+(define CN2 "Sisaket")
+
+(define (fn-for-city-name cn)
+  (... cn))
+
+;; Template rules used:
+;; - atomic non-distinct: String
