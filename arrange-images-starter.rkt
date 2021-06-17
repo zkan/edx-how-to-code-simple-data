78133ff4fedae0a4ce074ae5540e026bc01267ee
zkan <zkan.cs@gmail.com>
2021-06-17 21:29:34 +0700

    Implement layout-image func :bear:

--- arrange-images-starter.rkt
+++ arrange-images-starter.rkt
@@ -5,9 +5,9 @@
 
 ;; arrange-images-starter.rkt
 
-;; Data definitions
+;; Data definitions:
 
-;; ListOfImages is one of:
+;; ListOfImage is one of:
 ;;  - '()
 ;;  - (cons Image ListOfImage)
 ;; interp. a list of images
@@ -22,3 +22,51 @@
   (cond [(empty? loi) (...)]
         [else (... (first loi)
                    (fn-for-loi (rest loi)))]))
+
+;; Functions:
+
+;; ListOfImage -> Image
+;; lay out images left to right in increasing order of size
+
+;(check-expect (arrange-images '()) empty-image) ; not necessary
+(check-expect (arrange-images (cons (rectangle 10 20 "solid" "blue")
+                                    (cons (rectangle 20 30 "solid" "red")
+                                          '())))
+              (beside (rectangle 10 20 "solid" "blue")
+                      (rectangle 20 30 "solid" "red")
+                      empty-image))
+(check-expect (arrange-images (cons (rectangle 20 30 "solid" "red")
+                                    (cons (rectangle 10 20 "solid" "blue")
+                                          '())))
+              (beside (rectangle 10 20 "solid" "blue")
+                      (rectangle 20 30 "solid" "red")
+                      empty-image))
+
+;(define (arrange-images loi) empty-image) ;stub
+
+(define (arrange-images loi)
+  (layout-images (sort-images loi)))
+
+;; ListOfImage -> Image
+;; place image beside each other in order of list
+(check-expect (layout-images '()) empty-image)
+(check-expect (layout-images (cons (rectangle 10 20 "solid" "blue")
+                                   (cons (rectangle 20 30 "solid" "red")
+                                         '())))
+              (beside (rectangle 10 20 "solid" "blue")
+                      (rectangle 20 30 "solid" "red")
+                      empty-image))
+
+;(define (layout-images loi) empty-image) ;stub
+
+(define (layout-images loi)
+  (cond [(empty? loi) empty-image]
+        [else
+         (beside (first loi)
+                 (layout-images (rest loi)))]))
+
+;; ListOfImage -> ListOfImage
+;; sort images in increasing order of size
+;; !!!
+(define (sort-images loi) loi)
+
